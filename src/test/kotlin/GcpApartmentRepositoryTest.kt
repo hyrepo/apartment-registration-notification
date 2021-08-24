86e20a3cbc71e013f66e9a943510360d27c7ab59
Hao Yang <xz816111@outlook.com>
2021-08-24 19:52:06 +0800

    refactor: add test for db

---
+++ src/test/kotlin/GcpApartmentRepositoryTest.kt
@@ -0,0 +1,102 @@
+import com.google.cloud.firestore.Firestore
+import com.google.cloud.firestore.QueryDocumentSnapshot
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class GcpApartmentRepositoryTest {
+    @MockK(relaxed = true)
+    private lateinit var firestore: Firestore
+
+    private lateinit var gcpApartmentRepository: GcpApartmentRepository
+
+    @BeforeEach
+    internal fun setUp() {
+        MockKAnnotations.init(this)
+        gcpApartmentRepository = GcpApartmentRepository(firestore)
+    }
+
+    @Test
+    internal fun `should return true when verify if apartment exist given the apartment in DB`() {
+        every {
+            firestore.collection("apartments")
+                .whereEqualTo("date", "2020-01-01")
+                .whereEqualTo("district", "district A")
+                .whereEqualTo("type", "type A")
+                .whereEqualTo("name", "apartment A")
+                .get()
+                .get()
+                .documents
+        } returns listOf(object : QueryDocumentSnapshot(null, null, null, null, null, null) {})
+
+        assertTrue(
+            gcpApartmentRepository.hasApartment(
+                Apartment(
+                    date = "2020-01-01",
+                    district = "district A",
+                    type = "type A",
+                    name = "apartment A"
+                )
+            )
+        )
+    }
+
+    @Test
+    internal fun `should return true when verify if apartment exist given the apartment has multiple records in DB`() {
+        every {
+            firestore.collection("apartments")
+                .whereEqualTo("date", "2020-01-01")
+                .whereEqualTo("district", "district A")
+                .whereEqualTo("type", "type A")
+                .whereEqualTo("name", "apartment A")
+                .get()
+                .get()
+                .documents
+        } returns listOf(
+            object : QueryDocumentSnapshot(null, null, null, null, null, null) {},
+            object : QueryDocumentSnapshot(null, null, null, null, null, null) {}
+        )
+
+        assertTrue(
+            gcpApartmentRepository.hasApartment(
+                Apartment(
+                    date = "2020-01-01",
+                    district = "district A",
+                    type = "type A",
+                    name = "apartment A"
+                )
+            )
+        )
+    }
+
+    @Test
+    internal fun `should return false when verify if apartment exist given the apartment not in DB`() {
+        every {
+            firestore.collection("apartments")
+                .whereEqualTo("date", "2020-01-01")
+                .whereEqualTo("district", "district A")
+                .whereEqualTo("type", "type A")
+                .whereEqualTo("name", "apartment A")
+                .get()
+                .get()
+                .documents
+        } returns listOf()
+
+        assertFalse(
+            gcpApartmentRepository.hasApartment(
+                Apartment(
+                    date = "2020-01-01",
+                    district = "district A",
+                    type = "type A",
+                    name = "apartment A"
+                )
+            )
+        )
+    }
+
+
+}
